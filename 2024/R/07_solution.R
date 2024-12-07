0481b1f5402c5c6ac90dd96862aaca8c4f953c75
Michael Jones <michael.jones.gsy@gmail.com>
2024-12-07 13:05:01 +0000

    2024 day 7 - cleaned

--- 2024/R/07_solution.R
+++ 2024/R/07_solution.R
@@ -1,6 +1,14 @@
 library(tidyverse)
-options(digits = 22)
+options(digits = 22) # big numbers, so suppress R's default use of scientific notation
 INPUT_PATH <- "../input/07_input.txt"
+
+# This is a slow solution but I quite like it. Essentially all we're doing is
+# recursively constructing our calculation as we go along by trying all
+# two/three operators on the first two elements of the test vector, replacing
+# the first two with the running value of our calculation until we use them all
+# up. If the final result is the target, we output TRUE, if not, FALSE and crawl
+# back up the call stack. 
+
 
 # ---- input reading and parsing ----
 input <- readr::read_lines(INPUT_PATH)
@@ -13,10 +21,23 @@ parse_input <- function(input) {
 }
 
 # ---- Working ----
-# BFS through the combinations of operators 
+# The form of both parts is the same, the only difference is how we check
+# whether the text vector can construct the target, so we can pull out all the
+# scaffolding for handling the final total check into a function factory.
+make_part <- function(checker) {
+  function(input) {
+    parse_input(input) |>
+      mutate(test = map2_lgl(target, vec, checker, .progress = TRUE)) |>
+      filter(test) |>
+      pull(target) |>
+      sum()
+  }
+}
+
+# BFS through the combinations of operators for part 1
 check_vec <- function(target, vec, result = FALSE) {
-  if (length(vec) == 1) {
-    return(FALSE)
+  if (length(vec) <= 1) {
+    return(vec == target)
   }
   
   first_two <- head(vec, 2)
@@ -24,25 +45,16 @@ check_vec <- function(target, vec, result = FALSE) {
   
   add <- sum(first_two)
   mul <- prod(first_two)
-  
-  if (add == target || mul == target) {return(TRUE)}
   
   result || check_vec(target, c(add, rst)) || check_vec(target, c(mul, rst))
 }
 
 
-part_1 <- function(input) {
-  parse_input(input) |>
-    mutate(test = map2_lgl(target, vec, check_vec)) |>
-    filter(test) |>
-    pull(target) |>
-    sum()
-}
-
-# part 2 just introduces another operator
+# part 2 just introduces another operator, so a small tweak to the checker is
+# required.
 check_vec_2 <- function(target, vec, result = FALSE) {
-  if (length(vec) == 1) {
-    return(FALSE)
+  if (length(vec) <= 1) {
+    return(vec == target)
   }
   
   first_two <- head(vec, 2)
@@ -50,18 +62,7 @@ check_vec_2 <- function(target, vec, result = FALSE) {
   
   add <- sum(first_two)
   mul <- prod(first_two)
-  cat <- first_two[1] %|% first_two[2]
-  
-  # Additional check here required to ensure we're using the full vector
-  if (add == target & length(rst) == 0) {
-    return(TRUE)
-  }
-  if (mul == target & length(rst) == 0) {
-    return(TRUE)
-  }
-  if (cat == target & length(rst) == 0) {
-    return(TRUE)
-  }
+  cat <- as.numeric(paste(first_two, collapse = ""))
   
   result ||
     check_vec_2(target, c(add, rst)) ||
@@ -69,19 +70,8 @@ check_vec_2 <- function(target, vec, result = FALSE) {
     check_vec_2(target, c(cat, rst))
 }
 
-`%|%` <- function(l, r) {
-  as.numeric(paste0(l, r, collapse = ""))
-}
-
-
-part_2 <- function(input) {
-  parse_input(input) |>
-    mutate(test = map2_lgl(target, vec, check_vec_2, .progress = TRUE)) |>
-    filter(test) |>
-    pull(target) |>
-    sum()
-}
-
+part_1 <- make_part(check_vec)
+part_2 <- make_part(check_vec_2)
 
 # ---- Results ----
 part_1(input) # 1298300076754
