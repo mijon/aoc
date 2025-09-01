ef416ba0cb56a2fd32d63e5bb0cfa318ab2bbc6f
Michael Jones <michael.jones.gsy@gmail.com>
2025-09-01 21:53:35 +0100

    adds regression tests to day 2 for future

--- 2019/rust/day_02/src/main.rs
+++ 2019/rust/day_02/src/main.rs
@@ -7,3 +7,24 @@ fn main() {
     println!("part 1: {}", part_1::solve(&input)); // 2842648
     println!("part 2: {}", part_2::solve(&input)); // 9074
 }
+
+// Including tests to ensure that as we change the intcode lib, we don't introduce breaking
+// changes.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_works() {
+        let input = helpers::read_input("02").expect("unable to read input");
+        let result = part_1::solve(&input);
+        assert_eq!(result, 2842648);
+    }
+
+    #[test]
+    fn part_2_works() {
+        let input = helpers::read_input("02").expect("unable to read input");
+        let result = part_2::solve(&input);
+        assert_eq!(result, 9074);
+    }
+}
