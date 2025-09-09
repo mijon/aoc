aa261da2266242cd6afcabb9c6a92b4f0ffba8d1
Michael Jones <michael.jones.gsy@gmail.com>
2025-09-09 13:01:09 +0100

    improvements to day 1

--- 2019/rust/day_01/src/main.rs
+++ 2019/rust/day_01/src/main.rs
@@ -1,10 +1,8 @@
-use std::fs;
-
 mod part_1;
 mod part_2;
 
 fn main() {
-    let input = fs::read_to_string(INPUT_PATH).expect("Unable to read input file");
+    let input = helpers::read_input("01").expect("unable to read input");
 
     println!("part 1: {}", part_1::solve(&input));
     println!("part 2: {}", part_2::solve(&input));
