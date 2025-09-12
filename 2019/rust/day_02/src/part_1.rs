7d56fe989da94b35e6530ceaeaa65d05bfb0ecc2
Michael Jones <michael.jones.gsy@gmail.com>
2025-09-12 22:24:45 +0100

    Updates day 2 for changes made to day 5 intcode

--- 2019/rust/day_02/src/part_1.rs
+++ 2019/rust/day_02/src/part_1.rs
@@ -1,7 +1,7 @@
 use intcode::{IntcodeState, run_intcode};
 
-pub fn solve(input: &str) -> i64 {
-    let intcode = IntcodeState::new(&input)
+pub fn solve(input: &str) -> i32 {
+    let intcode = IntcodeState::new(&input, Vec::new())
         // We need to update the values in addresses 1 and 2.
         .edit_program(1, 12)
         .edit_program(2, 2);
--- 2019/rust/day_02/src/part_2.rs
+++ 2019/rust/day_02/src/part_2.rs
@@ -1,11 +1,11 @@
 use intcode::{IntcodeState, run_intcode};
 
-pub fn solve(input: &str) -> i64 {
+pub fn solve(input: &str) -> i32 {
     for noun in 1..99 {
         for verb in 1..99 {
-            let intcode = IntcodeState::new(&input);
-            let intcode = intcode.edit_program(1, noun as i64);
-            let intcode = intcode.edit_program(2, verb as i64);
+            let intcode = IntcodeState::new(&input, Vec::new());
+            let intcode = intcode.edit_program(1, noun as i32);
+            let intcode = intcode.edit_program(2, verb as i32);
 
             let result = run_intcode(intcode);
             if result.program[0] == 19690720 {
