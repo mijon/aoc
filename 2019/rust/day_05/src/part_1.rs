418c394121bd24f70ff104b469aa19d49916a0b2
Michael Jones <michael.jones.gsy@gmail.com>
2025-09-12 22:25:27 +0100

    2019 day 5 part 1

---
+++ 2019/rust/day_05/src/part_1.rs
@@ -0,0 +1,8 @@
+use intcode::{IntcodeState, run_intcode};
+
+pub fn solve(input: &str) -> i32 {
+    let intcode = IntcodeState::new(&input, vec![1]);
+
+    let mut result = run_intcode(intcode);
+    result.output.pop().expect("Expected output value")
+}
