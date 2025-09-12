804cd4aceaabd18931fa01f8397955d2566597a5
Michael Jones <michael.jones.gsy@gmail.com>
2025-09-12 23:15:27 +0100

    post clippy

--- 2019/rust/day_05/src/part_2.rs
+++ 2019/rust/day_05/src/part_2.rs
@@ -1,7 +1,7 @@
 use intcode::{IntcodeState, run_intcode};
 
 pub fn solve(input: &str) -> i32 {
-    let intcode = IntcodeState::new(&input, vec![5]);
+    let intcode = IntcodeState::new(input, vec![5]);
 
     let mut result = run_intcode(intcode);
     result.output.pop().expect("Expected output value")
