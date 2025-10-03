a71f55db3816fa09945596772a47e0955b0b0359
Michael Jones <michael.jones.gsy@gmail.com>
2025-10-03 19:33:48 +0100

    First run through updating types and interface for day 9

--- 2019/rust/intcode/src/lib.rs
+++ 2019/rust/intcode/src/lib.rs
@@ -5,19 +5,21 @@
 //! here and over time more features will be included.
 #![allow(dead_code)]
 
+use std::collections::HashMap;
 use std::str;
 /// Represents the state of the Intcode program
 #[derive(PartialEq, Debug, Clone)]
 pub struct IntcodeState {
     /// The vector of intcodes
-    pub program: Vec<i64>,
+    pub program: HashMap<i64, i64>,
     /// The 'current' position
-    pub head: usize,
+    pub head: i64,
     /// Whether the program has terminated (used for stopping evaluation)
     pub terminated: bool,
     // input: VecDeque<i64>,
     pub input: RepeatingInput,
     pub output: Vec<i64>,
+    pub relative_base: i64,
 }
 
 impl IntcodeState {
@@ -29,6 +31,7 @@ impl IntcodeState {
             // input: VecDeque::from(input),
             input: RepeatingInput::new(input),
             output: Vec::new(),
+            relative_base: 0,
         }
     }
 }
@@ -58,17 +61,23 @@ impl Iterator for RepeatingInput {
     }
 }
 
-fn prep_program(program: &str) -> Vec<i64> {
-    program
+fn prep_program(program: &str) -> HashMap<i64, i64> {
+    let values: Vec<i64> = program
         .split(',')
         .map(|n| n.trim().parse::<i64>().expect("unable to parse number"))
-        .collect()
+        .collect();
+    let mut output = HashMap::new();
+    for (pos, value) in values.iter().enumerate() {
+        output.insert(pos as i64, *value);
+    }
+    output
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum ParameterMode {
     Immediate,
     Position,
+    Relative,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -81,6 +90,7 @@ pub enum Opcode {
     JumpIfFalse(ParameterMode, ParameterMode),        // 6
     LessThan(ParameterMode, ParameterMode, ParameterMode), // 7
     Equals(ParameterMode, ParameterMode, ParameterMode), // 8
+    UpdateRelativeBase(ParameterMode),                //9
     Stop,                                             // 99
     Value(i64),
 }
@@ -119,6 +129,7 @@ pub fn num_params(o: Opcode) -> i32 {
         Opcode::Equals(..) => 3,
         Opcode::Stop => 0,
         Opcode::Value(_) => 1,
+        Opcode::UpdateRelativeBase(_) => 1,
     }
 }
 
@@ -168,6 +179,7 @@ pub fn parse_intcode(n: i64) -> Opcode {
             modes.pop().unwrap_or(ParameterMode::Position),
             modes.pop().unwrap_or(ParameterMode::Position),
         ),
+        9 => Opcode::UpdateRelativeBase(modes.pop().unwrap_or(ParameterMode::Position)),
         99 => Opcode::Stop,
         _ => Opcode::Value(n),
     }
@@ -199,7 +211,7 @@ where
 
 impl IntcodeState {
     pub fn step_intcode(mut self) -> Self {
-        let current_opcode = parse_intcode(self.program[self.head]);
+        let current_opcode = self.current_opcode();
         // println!(
         //     "{current_opcode:?}, input: {:?}, output: {:?}, prog: {:?}",
         //     self.input, self.output, self.program
@@ -207,38 +219,44 @@ impl IntcodeState {
         match current_opcode {
             Opcode::Add(m1, m2, m3) => {
                 let a = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => (self.head + 1) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let b = match m2 {
-                    ParameterMode::Position => self.program[self.head + 2],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 2),
                     ParameterMode::Immediate => (self.head + 2) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let target_pos = match m3 {
-                    ParameterMode::Position => self.program[self.head + 3] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 3),
                     ParameterMode::Immediate => self.head + 3,
+                    ParameterMode::Relative => todo!(),
                 };
-                self.program[target_pos] = self.program[a as usize] + self.program[b as usize];
+                self.set_opcode_num(target_pos, self.get_opcode_num(a) + self.get_opcode_num(b));
                 self.head += 4;
             }
             Opcode::Multiply(m1, m2, m3) => {
                 let a = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => (self.head + 1) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let b = match m2 {
-                    ParameterMode::Position => self.program[self.head + 2],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 2),
                     ParameterMode::Immediate => (self.head + 2) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let target_pos = match m3 {
-                    ParameterMode::Position => self.program[self.head + 3] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 3),
                     ParameterMode::Immediate => self.head + 3,
+                    ParameterMode::Relative => todo!(),
                 };
                 // println!(
                 //     "{} * {}",
                 //     self.program[a as usize], self.program[b as usize]
                 // );
-                self.program[target_pos] = self.program[a as usize] * self.program[b as usize];
+                self.set_opcode_num(target_pos, self.get_opcode_num(a) * self.get_opcode_num(b));
                 self.head += 4;
             }
             Opcode::Stop => {
@@ -251,102 +269,115 @@ impl IntcodeState {
                 // let input_val = self.input.pop_front().expect("Expected input!");
                 let input_val = self.input.next().expect("Expected input!");
                 let target_pos = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => self.head + 1,
+                    ParameterMode::Relative => todo!(),
                 };
-                self.program[target_pos] = input_val;
+                self.set_opcode_num(target_pos, input_val);
                 self.head += 2;
             }
             Opcode::Output(m1) => {
                 let target_pos = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => self.head + 1,
+                    ParameterMode::Relative => todo!(),
                 };
-                self.output.push(self.program[target_pos]);
+                self.output.push(self.get_opcode_num(target_pos));
                 self.head += 2;
             }
             Opcode::JumpIfTrue(m1, m2) => {
                 let a = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => (self.head + 1) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let b = match m2 {
-                    ParameterMode::Position => self.program[self.head + 2] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 2),
                     ParameterMode::Immediate => self.head + 2,
+                    ParameterMode::Relative => todo!(),
                 };
-                if self.program[a as usize] != 0 {
-                    self.head = self.program[b] as usize
+                if self.get_opcode_num(a) != 0 {
+                    self.head = self.get_opcode_num(b)
                 } else {
                     self.head += 3
                 };
             }
             Opcode::JumpIfFalse(m1, m2) => {
                 let a = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => (self.head + 1) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let b = match m2 {
-                    ParameterMode::Position => self.program[self.head + 2] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 2),
                     ParameterMode::Immediate => self.head + 2,
+                    ParameterMode::Relative => todo!(),
                 };
-                if self.program[a as usize] == 0 {
-                    self.head = self.program[b] as usize
+                if self.get_opcode_num(a) == 0 {
+                    self.head = self.get_opcode_num(b)
                 } else {
                     self.head += 3
                 };
             }
             Opcode::LessThan(m1, m2, m3) => {
                 let a = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => (self.head + 1) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let b = match m2 {
-                    ParameterMode::Position => self.program[self.head + 2],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 2),
                     ParameterMode::Immediate => (self.head + 2) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let c = match m3 {
-                    ParameterMode::Position => self.program[self.head + 3] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 3),
                     ParameterMode::Immediate => self.head + 4,
+                    ParameterMode::Relative => todo!(),
                 };
-                if self.program[a as usize] < self.program[b as usize] {
-                    self.program[c] = 1
+                if self.get_opcode_num(a) < self.get_opcode_num(b) {
+                    self.set_opcode_num(c, 1);
                 } else {
-                    self.program[c] = 0
+                    self.set_opcode_num(c, 0);
                 }
                 self.head += 4
             }
             Opcode::Equals(m1, m2, m3) => {
                 let a = match m1 {
-                    ParameterMode::Position => self.program[self.head + 1],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 1),
                     ParameterMode::Immediate => (self.head + 1) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let b = match m2 {
-                    ParameterMode::Position => self.program[self.head + 2],
+                    ParameterMode::Position => self.get_opcode_num(self.head + 2),
                     ParameterMode::Immediate => (self.head + 2) as i64,
+                    ParameterMode::Relative => todo!(),
                 };
                 let c = match m3 {
-                    ParameterMode::Position => self.program[self.head + 3] as usize,
+                    ParameterMode::Position => self.get_opcode_num(self.head + 3),
                     ParameterMode::Immediate => self.head + 4,
+                    ParameterMode::Relative => todo!(),
                 };
                 // println!("{:?}", self.head + 1);
                 // println!("{:?}", a);
                 // println!("{:?}", b);
                 // println!("{:?}", c);
-                if self.program[a as usize] == self.program[b as usize] {
-                    self.program[c] = 1
+                if self.get_opcode_num(a) == self.get_opcode_num(b) {
+                    self.set_opcode_num(c, 1);
                 } else {
-                    self.program[c] = 0
+                    self.set_opcode_num(c, 0);
                 }
                 self.head += 4
             }
+            Opcode::UpdateRelativeBase(parameter_mode) => todo!(),
         }
         self
     }
 }
 
 impl IntcodeState {
-    pub fn edit_program(mut self, idx: usize, val: i64) -> Self {
-        self.program[idx] = val;
+    pub fn edit_program(mut self, idx: i64, val: i64) -> Self {
+        self.set_opcode_num(idx, val);
         self
     }
 
@@ -355,8 +386,20 @@ impl IntcodeState {
         self
     }
 
+    pub fn get_opcode_num(&self, pos: i64) -> i64 {
+        *self.program.get(&pos).unwrap_or(&0)
+    }
+
+    pub fn get_opcode(&self, pos: i64) -> Opcode {
+        parse_intcode(self.get_opcode_num(pos))
+    }
+
+    pub fn set_opcode_num(&mut self, pos: i64, value: i64) {
+        self.program.insert(pos, value);
+    }
+
     pub fn current_opcode(&self) -> Opcode {
-        parse_intcode(self.program[self.head])
+        parse_intcode(self.get_opcode_num(self.head))
     }
 }
 
@@ -383,17 +426,22 @@ mod tests {
     #[test]
     fn test_prepping_program_works() {
         let result = prep_program("0,1,0");
-        assert_eq!(result, [0, 1, 0]);
+        let mut expected = HashMap::new();
+        expected.insert(0, 0);
+        expected.insert(1, 1);
+        expected.insert(2, 0);
+        assert_eq!(result, expected);
     }
 
     #[test]
     fn test_new_program_works() {
         let result = IntcodeState {
-            program: vec![1, 1, 1],
+            program: HashMap::from([(0, 1), (1, 1), (2, 1)]),
             head: 0,
             terminated: false,
             output: Vec::new(),
             input: RepeatingInput { values: vec![] },
+            relative_base: 0,
         };
         assert_eq!(IntcodeState::new("1,1,1", Vec::new()), result);
     }
@@ -406,7 +454,7 @@ mod tests {
 
     #[test]
     fn test_update_program() {
-        let result = vec![2, 0, 0, 0, 99];
+        let result = HashMap::from([(0, 2), (1, 0), (2, 0), (3, 0), (4, 99)]);
         let input = "1,0,0,0,99";
         let intcode_state = IntcodeState::new(input, Vec::new());
         let edited_program = intcode_state.edit_program(0, 2);
@@ -426,11 +474,11 @@ mod tests {
     }
 
     #[rstest]
-    #[case("1,0,0,0,99", vec![2, 0, 0, 0, 99])]
-    #[case("2,3,0,3,99", vec![2,3,0,6,99])]
-    #[case("2,4,4,5,99,0", vec![2,4,4,5,99,9801])]
-    #[case("1,1,1,4,99,5,6,0,99", vec![30,1,1,4,2,5,6,0,99])]
-    fn test_prog(#[case] input: &str, #[case] expected: Vec<i64>) {
+    #[case("1,0,0,0,99", HashMap::from([(0,2), (1,0), (2,0), (3,0), (4,99)]))]
+    #[case("2,3,0,3,99", HashMap::from([(0,2),(1,3),(2,0),(3,6),(4,99)]))]
+    #[case("2,4,4,5,99,0", HashMap::from([(0,2),(1,4),(2,4),(3,5),(4,99),(5,9801)]))]
+    #[case("1,1,1,4,99,5,6,0,99", HashMap::from([(0,30),(1,1),(2,1),(3,4),(4,2),(5,5),(6,6),(7,0),(8,99)]))]
+    fn test_prog(#[case] input: &str, #[case] expected: HashMap<i64, i64>) {
         let intcode_state = IntcodeState::new(input, Vec::new());
         let run_program = run_intcode(intcode_state);
         assert_eq!(expected, run_program.program)
@@ -492,5 +540,17 @@ mod tests {
         let intcode_state = IntcodeState::new(programstring, input);
         let run_program = run_intcode_until(intcode_state, |int_state| int_state.output.len() > 0);
         assert_eq!(10, run_program.output[0]);
+    }
+
+    #[test]
+    fn test_quine() {
+        let programstring = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99";
+        let expected = vec![
+            109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,
+        ];
+        let input = vec![];
+        let intcode_state = IntcodeState::new(programstring, input);
+        let run_program = run_intcode(intcode_state);
+        assert_eq!(expected, run_program.output)
     }
 }
